3311181e55e844852faf402bdbb52f3a270a7375
Andrey <andrey.boukreev@gamil.com>
2024-11-05 14:21:38 +0300

    test added

--- sample_background_service/app/src/main/java/ru/zoomparty/sample_background_service/PG.kt
+++ sample_background_service/app/src/main/java/ru/zoomparty/sample_background_service/PG.kt
@@ -3,32 +3,6 @@ package ru.zoomparty.sample_background_service
 fun main() {
     val phoneNumbers = getNumbers()
 
-    println("Номера телефонов, начинающиеся с +7:")
-    val filteredNumbers = phoneNumbers.filter { it.startsWith("+7") }
-    filteredNumbers.forEach { println(it) }
-
-    val uniqueNumbers = phoneNumbers.toSet()
-    println("Количество уникальных номеров: ${uniqueNumbers.size}")
-
-    val totalLength = phoneNumbers.sumOf { it.length }
-    println("Сумма длин всех номеров: $totalLength")
-
-    val namesByNumber = mutableMapOf<String, String>()
-    uniqueNumbers.forEach { number ->
-        print("Введите имя человека с номером телефона $number: ")
-        val name = readLine() ?: ""
-        namesByNumber[number] = name
-    }
-
-    namesByNumber.forEach { (number, name) ->
-        println("Абонент: $name. Номер телефона: $number")
-    }
-
-    namesByNumber.toSortedMap().forEach { (number, name) ->
-        println("Абонент: $name. Номер телефона: $number")
-    }
-    namesByNumber.toSortedMap(compareBy { (key,value)-> value })
-
 }
 
 fun getNumbers(): MutableList<String> {
